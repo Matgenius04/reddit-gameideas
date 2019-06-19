a23bd66940f17072cc2dfe282436de7710eb7ee0
drcrotato <31459944+drcrotato@users.noreply.github.com>
2019-06-19 16:50:45 -0400

    tech level and notif box

--- god-sim/main.js
+++ god-sim/main.js
@@ -12,6 +12,8 @@ let mouseX;
 let mouseY;
 let mouseDown = 0;
 
+let techLevel = 0;
+
 const mousevariable = {
     x: undefined,
     y: undefined
@@ -38,7 +40,7 @@ let scale = 1;
 let prevscale = 1;
 const translate = {
     x: 0,
-    y: 0
+    y: -1414.21356237
 };
 const prevtranslate = {
     x: 0,
@@ -94,7 +96,6 @@ function setup() {
     };
     window.addEventListener("contextmenu", (ev) => {
         ev.preventDefault();
-
         mousevariable.x = (1 / scale) * ((intran.a * ((ev.clientX - (c.width / 2)) - translate.x)) + (intran.c * ((-ev.clientY + (c.height / 2)) - translate.y)));
         mousevariable.y = (1 / scale) * ((intran.b * ((ev.clientX - (c.width / 2)) - translate.x)) + (intran.d * ((-ev.clientY + (c.height / 2)) - translate.y)));
         // mapTool = "land";
@@ -134,11 +135,9 @@ function setup() {
             y: ev.deltaY
         });
         scale -= ev.deltaY / 100;
-        translate.y += ev.deltaY;
     });
     window.requestAnimationFrame(frame);
 }
-
 function frame() {
     setTimeout(() => {
         window.requestAnimationFrame(frame);
@@ -156,7 +155,7 @@ function frame() {
     map.strokeStyle = lines;
     map.lineWidth = 2;
 
-    map.fillStyle = "#000000"
+    map.fillStyle = "#000000";
     map.beginPath();
     map.arc(mousevariable.x, mousevariable.y, mapc.width / 1028, 0, Math.PI * 2);
     map.fill();
@@ -211,5 +210,98 @@ function terraform() {
 
     mapData = map.getImageData(0, 0, mapc.width, mapc.height)
 }
+
+function notif(message) {
+    document.getElementById("notifMessage").innerHTML = message;
+    document.getElementById("notifBoxContainer").classList.add("showingContainer");
+
+    setTimeout(() => {
+        document.getElementById("notifBoxContainer").classList.remove("showingContainer");
+    }, 5000)
+}
+
+setInterval(() => {
+    techLevel++;
+
+    switch (techLevel) {
+        case 1:
+            notif("Humans have discovered: Fire<br> You better make sure physics is consistent");
+            break;
+
+        case 2:
+            notif("Humans have discovered: Hunting<br> Make sure to add animals for the humans to hunt");
+            break;
+
+        case 3:
+            notif("Humans have discovered: Farming<br> Make sure it rains occasionally");
+            break;
+
+        case 4:
+            notif("Humans have discovered: The Wheel<br> The loading time is too slow to accomadate faster travel, you should upgrade it");
+            break;
+
+        case 5:
+            notif("Humans have discovered: Cities<br> You should add more people to accomadate the requirements of a city");
+            break;
+
+        case 6:
+            notif("Humans have discovered: Writing<br> Humans need to be able to see well to read text,you should upgrade the machines rendered detail");
+            break;
+
+        case 7:
+            notif("Humans have discovered: Roads<br> You should make the world larger, if you can");
+            break;
+
+        case 8:
+            notif("Humans have discovered: Math<br> Now humans can model how the world works accurately. If something is out of whack, they will grow suspicious.");
+            break;
+
+        case 9:
+            notif("Humans have discovered: Sailing<br> You should make the world larger and add more people, so they don't sail to oblivion, and increase suspicion");
+            break;
+
+        case 10:
+            notif("Humans have discovered: Guns<br> You don't have to do anything, their suspicion will go down, since the humans are too busy murdering each other to give a crap");
+            break;
+
+        case 11:
+            notif("Humans have discovered: Telescopes<br> You should add more planets for the humans to observe");
+            break;
+        
+        case 12:
+            notif("Humans have discovered: Industry<br> The Industrial Revolution is in full swing now, you will have to add land and people to accomadate the growth");
+            break;
+
+        case 13:
+            notif("Humans have discovered: Airplanes<br> People are moving so fast now that the computer will need another upgrade to increase loading speeds");
+            break;
+
+        case 14:
+            notif("Humans have discovered: Quantum Physics<br> Suspicion goes up, because ..... quantum physics");
+            break;
+
+        case 15:
+            notif("Humans have discovered: Nuclear Bombs<br> Suspicion goes down again, since everyone is busier trying to murder each other");
+            break;
+
+        case 16:
+            notif("Humans have discovered: Space Flight<br> Since humans have never been able to go to space, the physics is a little messed up. You need to fix it");
+            break;
+
+        case 17:
+            notif("Humans have discovered: The Internet<br> Suspicion goes up, due to the speard of conspiracy theories");
+            break;
+
+        case 18:
+            notif("Humans have discovered: Space Colonization<br> You will need to make maps for those other planets you made earlier now");
+            break;
+
+        case 19:
+            notif("You won the game! Quite noice");
+            break;
+    }
+
+
+}, 60000)
 
 setup();
